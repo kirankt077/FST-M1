a3dbcc3436f17c43748d293f8a9ea0fc48e44f99
Kiran Thakur <57950467+kirankt077@users.noreply.github.com>
2021-08-02 12:39:25 +0530

    Add files via upload

---
+++ Selenium/TestNG/Activity_2.java
@@ -0,0 +1,66 @@
+package TestNG_Activities;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Activity_2 {
+    WebDriver driver;
+    
+    @BeforeTest
+    public void beforeMethod() {
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+            
+        //Open the browser
+        driver.get("https://www.training-support.net/selenium/target-practice");
+    }
+    
+    @Test
+    public void testCase1() {
+        //This test case will pass
+        String title = driver.getTitle();
+        System.out.println("Title is: " + title);
+        Assert.assertEquals(title, "Target Practice");
+    }
+    
+    @Test
+    public void testCase2() {
+        //This test case will Fail
+        WebElement blackButton = driver.findElement(By.cssSelector("button.black"));
+        Assert.assertTrue(blackButton.isDisplayed());
+        Assert.assertEquals(blackButton.getText(), "Black");
+    }
+    
+    @Test(enabled = false)
+    public void testCase3() {
+        //This test will be skipped and not counted
+        String subHeading = driver.findElement(By.className("sub")).getText();
+        Assert.assertTrue(subHeading.contains("Practice"));
+    }
+    
+    @Test
+    public void testCase4() {
+        //This test will be skipped and will be be shown as skipped
+        throw new SkipException("Skipping test case");      
+    }
+
+    @AfterTest
+    public void afterMethod() {
+        //Close the browser
+        driver.close();
+    }
+
+}
---
+++ Selenium/TestNG/SampleTest.java
@@ -0,0 +1,13 @@
+package TestNG_Activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	 @Test
+	    public void loginTest() {
+	        System.out.println("Run Test");
+}
+}
