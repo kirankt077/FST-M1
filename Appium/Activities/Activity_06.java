28e45377da059f1035989b3351c5e7268db9d009
Kiran Thakur <57950467+kirankt077@users.noreply.github.com>
2021-08-18 12:55:35 +0530

    Add files via upload

---
+++ Appium/Activities/Activity_06.java
@@ -0,0 +1,81 @@
+package Appium_Project;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Activity_06 {
+    AppiumDriver<MobileElement> driver = null;
+    WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Pixel 4 API 28");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appPackage", "com.android.chrome");
+        caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        caps.setCapability("noReset", true);
+
+        // Instantiate Appium Driver
+        URL appServer = new URL("http://0.0.0.0:4723/wd/hub");
+        driver = new AndroidDriver<MobileElement>(appServer, caps);
+        wait = new WebDriverWait(driver, 5);
+
+        driver.get("https://www.training-support.net/selenium/lazy-loading");
+    }
+
+    @Test
+    public void imageScrollTest() throws InterruptedException {
+        // wait for page to load
+    	Thread.sleep(10000);
+       // MobileElement pageTitle = driver.findElementByClassName("android.widget.TextView");
+        
+        //String pageTitle = driver.findElementByXPath("//android.view.View[@text='Lazy Loading']").getText();
+        
+    	//String pageTitle = driver.getTitle();
+        //wait.until(ExpectedConditions.textToBePresentInElement(pageTitle, "Lazy Loading"));
+
+        // Count the number of images shown on the screen
+        List<MobileElement> numberOfImages = driver.findElementsByXPath("//android.view.View/android.view.View/android.widget.Image");
+        System.out.println("Number of image shown currently: " + numberOfImages.size());
+        
+        // Assertion before scrolling
+        Assert.assertEquals(numberOfImages.size(), 2);
+        
+        // Scroll to Helen's post
+        driver.findElement(MobileBy.AndroidUIAutomator("UiScrollable(UiSelector()).scrollTextIntoView(\"ade\")"));
+        
+        // Find the number of images shown after scrolling
+        numberOfImages = driver.findElementsByXPath("//android.view.View/android.view.View/android.widget.Image");
+        System.out.println("Number of image shown currently: " + numberOfImages.size());
+        
+        // Assertion after scrolling
+        Assert.assertEquals(numberOfImages.size(), 2);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
